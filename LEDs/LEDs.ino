cfd4e7a6ec0ffb2747f6b8b044beeb5b32c5567a
Aurelia <aurelia.haas@mail.mcgill.ca>
2023-03-06 18:37:39 +0000

    Led for ESP32 Flashing

--- LEDs/LEDs.ino
+++ LEDs/LEDs.ino
@@ -1,25 +1,39 @@
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/vs-code-platformio-ide-esp32-esp8266-arduino/
-*********/
+// /*********
+//   Rui Santos
+//   Complete project details at https://RandomNerdTutorials.com/vs-code-platformio-ide-esp32-esp8266-arduino/
+// *********/
 
-#include <Arduino.h>
+// #include <Arduino.h>
 
-#define LED 2
+// #define LED 1
 
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(115200);
+//   pinMode(LED, OUTPUT);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   digitalWrite(LED, HIGH);
+//   Serial.println("LED is on");
+//   delay(1000);
+//   digitalWrite(LED, LOW);
+//   Serial.println("LED is off");
+//   delay(1000);
+// }
+
+
+//LED BUILT_IN is GPIO 33 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(LED, OUTPUT);
+  pinMode(33, OUTPUT); // Set the pin as output
 }
 
+// Remember that the pin work with inverted logic
+// LOW to Turn on and HIGH to turn off
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  Serial.println("LED is on");
-  delay(1000);
-  digitalWrite(LED, LOW);
-  Serial.println("LED is off");
-  delay(1000);
+  digitalWrite(33, LOW); //Turn on
+  delay (1000); //Wait 1 sec
+  digitalWrite(33, HIGH); //Turn off
+  delay (1000); //Wait 1 sec
 }
-

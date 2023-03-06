3338e26b474df04de4e54d3ecd0ede32b45c5e1c
Aurelia <aurelia.haas@mail.mcgill.ca>
2023-03-06 18:40:34 +0000

    Update LEDs.ino

--- LEDs/LEDs.ino
+++ LEDs/LEDs.ino
@@ -1,29 +1,3 @@
-// /*********
-//   Rui Santos
-//   Complete project details at https://RandomNerdTutorials.com/vs-code-platformio-ide-esp32-esp8266-arduino/
-// *********/
-
-// #include <Arduino.h>
-
-// #define LED 1
-
-// void setup() {
-//   // put your setup code here, to run once:
-//   Serial.begin(115200);
-//   pinMode(LED, OUTPUT);
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-//   digitalWrite(LED, HIGH);
-//   Serial.println("LED is on");
-//   delay(1000);
-//   digitalWrite(LED, LOW);
-//   Serial.println("LED is off");
-//   delay(1000);
-// }
-
-
 //LED BUILT_IN is GPIO 33 
 void setup() {
   pinMode(33, OUTPUT); // Set the pin as output

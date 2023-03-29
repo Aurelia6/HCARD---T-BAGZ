abaaee422ea4a2dc919b4ffc41417ff4692e627e
Aurelia <aurelia.haas@mail.mcgill.ca>
2023-03-29 20:29:37 +0200

    LED Comments

--- LED_Arduino/led/led.ino
+++ LED_Arduino/led/led.ino
@@ -1,8 +1,6 @@
-// LAB8 - 讀取光敏電阻 (v2)
-
-int photocellPin = 2; // 光敏電阻 (photocell) 接在 anallog pin 2
+int photocellPin = 2; // (photocell) analog pin 2
 int photocellVal = 0; // photocell variable
-int minLight = 650;   // 最小光線門檻值
+int minLight = 650;   // Threshold
 int ledPin_1 = 9;
 int ledPin_2 = 8;
 int ledState = 0; 
@@ -14,18 +12,18 @@ void setup() {
 }
 
 void loop() {
-  // 讀取光敏電阻並輸出到 Serial Port 
+  // Serial Port 
   photocellVal = analogRead(photocellPin);
   Serial.println(photocellVal);   
   
-  // 光線不足時打開 LED
+  // LED
   if (photocellVal < minLight && ledState == 0) {
     digitalWrite(ledPin_1, HIGH); // turn on LED
     digitalWrite(ledPin_2, HIGH);
     ledState = 1;
   }
   
-  // 光線充足時關掉 LED
+  // LED
   if (photocellVal > minLight && ledState == 1) {
     digitalWrite(ledPin_1, LOW); // turn off LED
     digitalWrite(ledPin_2, LOW);
